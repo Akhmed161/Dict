83b3eaceecf4593734bc8e637ae0add31907440a
gaytukaeva <129388118+gaytukaeva@users.noreply.github.com>
2023-06-21 16:57:44 +0300

    Update keywords.py
    ', 'имей'],

--- keywords.py
+++ keywords.py
@@ -502,7 +502,7 @@ pattern = {
     'гдз': ['gdz'],
     'кибер герои': ['cyberHero Cups', 'cyberHero', 'cyber hero'],
     'бум': ['boom'],
-    'имэй': ['imai', 'imei', 'imey'],
+    'имэй': ['imai', 'imei', 'imey', 'имей'],
     'спотифай': ['Spotify', 'spoti fy'],
     'билайн': ['beeline', 'белайн'],
     'йота': ['yota'],
